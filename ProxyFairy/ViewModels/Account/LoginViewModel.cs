4e9ff735f6afc1a66028b12e577026f4a0219f71
Łukasz Bednarek <bednarek.lukasz@gmail.com>
2019-01-07 18:38:25 +0100

    add Views folder
    prepared layout/ view imports

---
+++ ProxyFairy/ViewModels/Account/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProxyFairy.ViewModels.Account
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [UIHint("email")]
+        public string Email { get; set; }
+        [Required]
+        [UIHint("password")]
+        public string Password { get; set; }
+    }
+}
