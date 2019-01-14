e3d4f2570cb93d0ee0974f024d8005d9e6eb4ead
Łukasz Bednarek <bednarek.lukasz@gmail.com>
2019-01-14 21:48:42 +0100

    add admin controller
    prepared simple admin controller, possible to create new users

---
+++ ProxyFairy/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProxyFairy.Core.Model;
+using ProxyFairy.ViewModels.Admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProxyFairy.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> _userManager;
+
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public ViewResult Index() => View(_userManager.Users);
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser
+                {
+                    UserName = model.Name,
+                    Email = model.Email
+                };
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+    }
+}
