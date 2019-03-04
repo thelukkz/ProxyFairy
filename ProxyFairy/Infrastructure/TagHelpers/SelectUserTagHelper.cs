0fcf03886fd8806a10feac2d084f4d1c8171d90f
Łukasz Bednarek <bednarek.lukasz@gmail.com>
2019-03-04 21:59:35 +0100

    add create customer post method
    new create customer post method/ new select application user tag helper

---
+++ ProxyFairy/Infrastructure/TagHelpers/SelectUserTagHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.EntityFrameworkCore;
+using ProxyFairy.Core.Model;
+using ProxyFairy.Core.Repository.Abstract;
+
+namespace ProxyFairy.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement("select", Attributes = "model-for")]
+    public class SelectUserTagHelper : TagHelper
+    {
+        private IRepository _repository;
+
+        public SelectUserTagHelper(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ModelExpression ModelFor { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.AppendHtml((await output.GetChildContentAsync(false)).GetContent());
+
+            string selected = ModelFor.Model as string;
+
+            var appUsers = await _repository.All<AppUser>().ToListAsync();
+            foreach (var user in appUsers)
+            {
+                if (selected != null && selected.Equals(user.Id))
+                {
+                    output.Content.AppendHtml($"<option selected value={user.Id}>{user.UserName}</option>");
+                }
+                else
+                {
+                    output.Content.AppendHtml($"<option value={user.Id}>{user.UserName}</option>");
+                }
+            }
+            output.Attributes.SetAttribute("Name", ModelFor.Name);
+            output.Attributes.SetAttribute("Id", ModelFor.Name);
+        }
+    }
+}
